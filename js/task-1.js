ad6a49c0771599a38e1aa58c908e451954fd0098
suzanna-samoilenko <fb.alferova.s@gmail.com>
2024-09-24 00:21:34 +0300

    Initial commit

---
+++ js/task-1.js
@@ -0,0 +1,12 @@
+function slugify(title) {
+  const normalizedMessage = title.toLowerCase();
+  const arr = normalizedMessage.split(' ');
+  const message = arr.join('-');
+
+  return message;
+}
+
+console.log(slugify('Arrays for beginners')); // "arrays-for-beginners"
+console.log(slugify('English for developer')); // "english-for-developer"
+console.log(slugify('Ten secrets of JavaScript')); // "ten-secrets-of-javascript"
+console.log(slugify('How to become a JUNIOR developer in TWO WEEKS')); // "how-to-become-a-junior-developer-in-two-weeks"
